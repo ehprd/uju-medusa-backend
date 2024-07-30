297a84dcdbc47d4e9bd5c0a3bb53b035876a596d
re4rk <37167652+re4rk@users.noreply.github.com>
2024-07-30 21:23:37 +0900

    Add new plugins and dependencies for Babel and Medusa
    Updated .babelrc.js to include new plugins for decorators and class properties. Augmented medusa-config.js with Google authentication and PayPal payment plugins. Added missing Babel and Medusa dependencies to package.json, and created a new middleware file for managing user login and product queries in the Medusa API.

--- .babelrc.js
+++ .babelrc.js
@@ -3,10 +3,16 @@ let ignore = [`**/dist`]
 // Jest needs to compile this code, but generally we don't want this copied
 // to output folders
 if (process.env.NODE_ENV !== `test`) {
-  ignore.push(`**/__tests__`)
+    ignore.push(`**/__tests__`)
 }
 
 module.exports = {
-  presets: [["babel-preset-medusa-package"], ["@babel/preset-typescript"]],
-  ignore,
+    presets: [["babel-preset-medusa-package"], ["@babel/preset-env"], ["@babel/preset-typescript"]],
+    plugins: [
+        ["@babel/plugin-proposal-decorators", { "version": "legacy" }],
+        ["@babel/plugin-transform-class-properties", { "loose": true }],
+        ["@babel/plugin-transform-private-methods", { "loose": true }],
+        ["@babel/plugin-transform-private-property-in-object", { "loose": true }]
+    ],
+    ignore,
 }
--- medusa-config.js
+++ medusa-config.js
@@ -17,6 +17,13 @@ switch (process.env.NODE_ENV) {
     break;
 }
 
+const BACKEND_URL = process.env.BACKEND_URL || "localhost:9000"
+const ADMIN_URL = process.env.ADMIN_URL || "localhost:7000"
+const STORE_URL = process.env.STORE_URL || "localhost:8000"
+
+const GoogleClientId = process.env.GOOGLE_CLIENT_ID || ""
+const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET || ""
+
 try {
   dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
 } catch (e) {}
@@ -29,9 +36,9 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
+  process.env.DATABASE_URL || "postgres://postgres:password//localhost:5432/medusa";
 
-const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6381";
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -52,10 +59,59 @@ const plugins = [
       },
     },
   },
+  {
+    resolve: "medusa-plugin-auth",
+    /** @type {import('medusa-plugin-auth').AuthOptions} */
+    options: [
+      {
+        type: "google",
+        // strict: "all", // or "none" or "store" or "admin"
+        strict: "none",
+        identifier: "google",
+        clientID: GoogleClientId,
+        clientSecret: GoogleClientSecret,
+        admin: {
+          callbackUrl: `${BACKEND_URL}/admin/auth/google/cb`,
+          failureRedirect: `${ADMIN_URL}/login`,
+          // The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url` to the auth url
+          // This query param will have the priority over this configuration
+          successRedirect: `${ADMIN_URL}/`
+          // authPath: '/admin/auth/google',
+          // authCallbackPath: '/admin/auth/google/cb',
+          // expiresIn: 24 * 60 * 60 * 1000,
+          // verifyCallback: (container, req, accessToken, refreshToken, profile, strict) => {
+          //    // implement your custom verify callback here if you need it
+          // },
+        },
+        store: {
+          callbackUrl: `${BACKEND_URL}/store/auth/google/cb`,
+          failureRedirect: `${STORE_URL}/login`,
+          // The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url` to the auth url
+          // This query param will have the priority over this configuration
+          successRedirect: `${STORE_URL}/`
+          // authPath: '/store/auth/google',
+          // authCallbackPath: '/store/auth/google/cb',
+          // expiresIn: 24 * 60 * 60 * 1000,
+          // verifyCallback: (container, req, accessToken, refreshToken, profile, strict) => {
+          //    // implement your custom verify callback here if you need it
+          // },
+        }
+      }
+    ]
+  },
+  {
+    resolve: `medusa-payment-paypal`,
+    options: {
+      sandbox: process.env.PAYPAL_SANDBOX,
+      client_id: process.env.PAYPAL_CLIENT_ID,
+      client_secret: process.env.PAYPAL_CLIENT_SECRET,
+      auth_webhook_id: process.env.PAYPAL_AUTH_WEBHOOK_ID,
+    },
+  },
 ];
 
 const modules = {
-  /*eventBus: {
+  eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL
@@ -66,18 +122,18 @@ const modules = {
     options: {
       redisUrl: REDIS_URL
     }
-  },*/
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
 const projectConfig = {
   jwt_secret: process.env.JWT_SECRET || "supersecret",
   cookie_secret: process.env.COOKIE_SECRET || "supersecret",
-  store_cors: STORE_CORS,
   database_url: DATABASE_URL,
+  store_cors: STORE_CORS,
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
-  // redis_url: REDIS_URL
+  redis_url: REDIS_URL
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule} */
