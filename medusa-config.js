7e10abe4759709b85cb0cc985ce03229f1209779
re4rk <37167652+re4rk@users.noreply.github.com>
2024-07-30 19:20:56 +0900

    Add inventory and stock location services
    This commit integrates the `@medusajs/inventory` and `@medusajs/stock-location` services into the medusa configuration file to enhance inventory and stock location management. This change aims to improve backend capabilities for handling inventory operations.

--- medusa-config.js
+++ medusa-config.js
@@ -123,6 +123,12 @@ const modules = {
       redisUrl: REDIS_URL
     }
   },
+  inventoryService: {
+    resolve: "@medusajs/inventory",
+  },
+  stockLocationService: {
+    resolve: "@medusajs/stock-location",
+  },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
@@ -131,6 +137,7 @@ const projectConfig = {
   cookie_secret: process.env.COOKIE_SECRET || "supersecret",
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
+
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL
