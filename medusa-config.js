4faebd512cd9d66ae030c723afbf26eca218df4a
re4rk <37167652+re4rk@users.noreply.github.com>
2024-07-31 13:38:28 +0900

    Simplify DATABASE_URL connection string
    Remove default username and password from DATABASE_URL connection string for better security. This ensures that the connection relies on environment-specific credentials.

--- medusa-config.js
+++ medusa-config.js
@@ -36,7 +36,7 @@ const ADMIN_CORS =
 const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
 const DATABASE_URL =
-  process.env.DATABASE_URL || "postgres://postgres:password//localhost:5432/medusa";
+  process.env.DATABASE_URL || "postgres://localhost:5432/medusa";
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6381";
 
